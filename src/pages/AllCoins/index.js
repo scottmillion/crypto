3f80b5ca4ad95f1bdb26d3bb507a94acda38f0a0
Scott Million <scottmillion@gmail.com>
2021-08-07 16:03:40 +0700

    first commit

---
+++ src/pages/AllCoins/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class AllCoins extends React.Component {
+  
+
+    render() {
+      
+      
+    return (
+      <>
+        <div><h1>This is the All Coins Page</h1></div>
+      </>
+      
+    )
+  }
+}
+
+export default AllCoins;
