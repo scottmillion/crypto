a675fbde806926a95c3d73bdf0ad8b8562f5eb8d
Scott Million <scottmillion@gmail.com>
2021-08-08 08:47:46 +0700

    testing api

--- src/pages/AllCoins/index.js
+++ src/pages/AllCoins/index.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import axios from 'axios'
 
 class AllCoins extends React.Component {
+  state: {
+    data = null;
+  }
+
+  componentDidMount = async () => {
+    try {
+      const { data } = await axios('https://api.coingecko.com/api/v3/global')
+      this.setState({ data })
+    } catch {
+
+    }
+  }
+
   render() {
     return (
-      <div>
-        <h1>This is the All Coins Page</h1>
-      </div>
+      <>
+        <div>
+          {if (this.state.data) {
+            <h2>Made it</h2>
+          }}
+          <h1>This is the All Coins Page</h1>
+        </div>
+      </>
     )
   }
 }
