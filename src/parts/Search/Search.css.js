3ccf8acd49ed4a156d5772c202c5b5bba7b76d8b
Scott Million <scottmillion@gmail.com>
2021-08-23 14:08:19 +0700

    Added Suggested Search Features

--- src/parts/Search/Search.css.js
+++ src/parts/Search/Search.css.js
@@ -1,24 +1,41 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const Input = styled.input`
-    background: ${(props) => props.theme.tertiary};
+  background: ${(props) => props.theme.tertiary};
+  color: inherit;
+  font-size: 1.1rem;
+  width: ${(props) => props.width}px;
+  height: 63px;
+  padding: 0px 0px 0px 60px;
+  margin-right: 27px;
+  border: none;
+  border-radius: 20px;
+
+  &::placeholder {
     color: inherit;
-    width: 450px;
-    height: 63px;
-    padding: 0;
-    padding-left: 60px;
-    margin-right: 27px;
-    border: 2px solid ${(props) => props.theme.secondary};
-    border-radius: 20px;
     font-size: 1.1rem;
-    
-    &::placeholder {
-      color: inherit;
-      font-size: 1.1rem;
-    }
-    &:focus {
-      outline: none;
-      border: 2px solid #737373;
-      border-radius: 20px;
-    }
-`;
+  }
+  &:focus {
+    outline: none;
+    /* border: 2px solid #737373;
+    border-radius: 20px; */
+  }
+`
+
+export const SearchList = styled.div`
+  position: absolute;
+  background: ${(props) => props.theme.tertiary};
+  color: inherit;
+  padding: 0px 20px 0px 20px;
+  margin-left: 42px;
+  font-size: 1rem;
+  z-index: 10000000;
+`
+
+export const SearchListItem = styled.div`
+  padding: 2px 0px;
+  z-index: Infinity;
+  &:hover {
+    background: ${(props) => props.theme.primary};
+  }
+`
