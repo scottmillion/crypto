230736ec6878c6a0ab301ccd00ac025ab9a63e5d
Scott Million <scottmillion@gmail.com>
2021-08-23 22:02:16 +0700

    Temp delete to change folder case

--- src/assets/images/index.js
+++
@@ -1,6 +0,0 @@
-export const NavBarImages = {
-  searchIcon: require('./search-icon.png').default,
-  searchIconLight: require('./search-icon-light.png').default,
-  themeIcon: require('./theme-icon.png').default,
-  themeIconLight: require('./theme-icon-light.png').default,
-}
