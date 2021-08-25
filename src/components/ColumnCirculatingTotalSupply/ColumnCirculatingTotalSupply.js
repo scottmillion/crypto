3ee9c41e57ee19a4c7872d694b219d89b7cb935a
Scott Million <scottmillion@gmail.com>
2021-08-26 04:08:06 +0700

    Changed function name

--- src/components/ColumnCirculatingTotalSupply/ColumnCirculatingTotalSupply.js
+++ src/components/ColumnCirculatingTotalSupply/ColumnCirculatingTotalSupply.js
@@ -1,18 +1,18 @@
 import { formatCurrency } from '@coingecko/cryptoformat'
 import { Cell, ColumnPercentBar } from 'components'
-import { shorterNumber } from 'utils'
+import { numberShortener } from 'utils'
 
 const ColumnCirculatingTotalSupply = (props) => (
   <Cell width={props.width}>
     <ColumnPercentBar
       color1={props.color1}
       color2={props.color2}
-      first={shorterNumber(
+      first={numberShortener(
         formatCurrency(props.circulatingSupply, props.currency, 'en'),
       ).slice(1)}
       second={
         (props.totalSupply &&
-          shorterNumber(
+          numberShortener(
             formatCurrency(props.totalSupply, props.currency, 'en'),
           ).slice(1)) || <span>&#8734;</span>
       }
--- src/components/ColumnVolumeMarketCap/ColumnVolumeMarketCap.js
+++ src/components/ColumnVolumeMarketCap/ColumnVolumeMarketCap.js
@@ -1,16 +1,16 @@
 import { formatCurrency } from '@coingecko/cryptoformat'
 import { Cell, ColumnPercentBar } from 'components'
-import { shorterNumber } from 'utils'
+import { numberShortener } from 'utils'
 
 const ColumnVolumeMarketCap = (props) => (
   <Cell width={props.width}>
     <ColumnPercentBar
       color1={props.color1}
       color2={props.color2}
-      first={shorterNumber(
+      first={numberShortener(
         formatCurrency(props.totalVolume, props.currency, 'en'),
       )}
-      second={shorterNumber(
+      second={numberShortener(
         formatCurrency(props.marketCap, props.currency, 'en'),
       )}
       percent={(100 * props.totalVolume) / props.marketCap}
