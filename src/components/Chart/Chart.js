4f42682a0d13acf3523411581f33c0e668c848a0
Scott Million <scottmillion@gmail.com>
2021-09-02 09:35:53 +0700

    Modified Charts so it no longer requires state or query selectors

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,34 +1,55 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { Bar, Line } from 'react-chartjs-2'
 import { withTheme } from 'styled-components'
 
 const Chart = (props) => {
-  const [gradient, setGradient] = useState('')
-  useEffect(() => {
+  const data = (canvas) => {
+    const ctx = canvas.getContext('2d')
+    const data = {
+      labels: props.dataLabels,
+      datasets: [
+        {
+          label: props.label,
+          data: props.dataPoints,
+        },
+      ],
+    }
+    const dataset = data.datasets[0]
+
     if (props.type === 'Line') {
-      const canvas = document.querySelector('#bitcoin-line-chart')
-      var ctx = canvas.getContext('2d')
-      var gradient = ctx.createLinearGradient(0, 0, 0, 400)
+      const gradient = ctx.createLinearGradient(0, 0, 0, 400)
       gradient.addColorStop(0, props.theme.lineChartGradientTop)
       gradient.addColorStop(1, props.theme.lineChartGradientBottom)
-      setGradient(gradient)
+      dataset.fill = true
+      dataset.backgroundColor = gradient
+      dataset.borderColor = props.theme.lineChart
+      options.elements = {
+        point: {
+          radius: 0,
+        },
+        line: {
+          tension: 0.4,
+        },
+      }
+      options.scales.x.ticks = {
+        autoSkip: true,
+        maxTicksLimit: 14,
+        maxRotation: 0,
+        minRotation: 0,
+      }
     }
-    if (props.type === 'Bar') {
-      const canvas = document.querySelector('#bitcoin-bar-chart')
-      canvas.height = 134
-    }
-    // eslint-disable-next-line
-  }, [])
 
-  const data = {
-    labels: props.dataLabels,
-    datasets: [
-      {
-        label: props.label,
-        data: props.dataPoints,
-      },
-    ],
+    if (props.type === 'Bar') {
+      dataset.barPercentage = 0.93
+      dataset.backgroundColor = props.theme.barChart
+      dataset.borderRadius = 4
+      dataset.borderWidth = 0
+      dataset.borderSkipped = false
+    }
+
+    return data
   }
+
   const options = {
     plugins: {
       legend: {
@@ -55,43 +76,10 @@ const Chart = (props) => {
     },
   }
 
-  const dataset = data.datasets[0]
-
-  if (props.type === 'Bar') {
-    dataset.barPercentage = 0.93
-    dataset.backgroundColor = props.theme.barChart
-    dataset.borderRadius = 4
-    dataset.borderWidth = 0
-    dataset.borderSkipped = false
-  }
-  if (props.type === 'Line') {
-    dataset.fill = true
-    dataset.backgroundColor = gradient
-    dataset.borderColor = props.theme.lineChart
-    options.elements = {
-      point: {
-        radius: 0,
-      },
-      line: {
-        tension: 0.4,
-      },
-    }
-    options.scales.x.ticks = {
-      autoSkip: true,
-      maxTicksLimit: 14,
-      maxRotation: 0,
-      minRotation: 0,
-    }
-  }
-
   return (
     <>
-      {props.type === 'Line' && (
-        <Line data={data} options={options} id="bitcoin-line-chart" />
-      )}
-      {props.type === 'Bar' && (
-        <Bar data={data} options={options} id="bitcoin-bar-chart" />
-      )}
+      {props.type === 'Line' && <Line data={data} options={options} />}
+      {props.type === 'Bar' && <Bar data={data} options={options} />}
     </>
   )
 }
