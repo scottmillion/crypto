aab74663d63d873b68db37e22a4bdb0a969314a5
Scott Million <scottmillion@gmail.com>
2021-10-12 14:21:08 +0700

    Added css for no selecting text

--- src/components/Search/Search.css.js
+++ src/components/Search/Search.css.js
@@ -41,4 +41,10 @@ export const SearchListItem = styled.div`
   &:hover {
     background: ${(props) => props.theme.primary};
   }
+  -webkit-touch-callout: none; /* iOS Safari */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Old versions of Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+  user-select: none; /* Non-prefixed version, currently supported by Chrome, Edge, Opera and Firefox */
 `
