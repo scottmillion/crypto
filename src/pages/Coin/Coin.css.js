d1fb6438a8006c2065f52cb024741480d60bebb4
Scott Million <scottmillion@gmail.com>
2021-10-28 10:14:44 +0700

    Refactored by creating new components elsewhere

--- src/pages/Coin/Coin.css.js
+++ src/pages/Coin/Coin.css.js
@@ -1,113 +1,28 @@
 import styled from 'styled-components'
 
-export const BluePlus = styled.span`
-  background: #2172e5;
-  color: white;
-  padding-inline: 5px;
-  margin-right: 10px;
-  border-radius: 4px;
-  &:after {
-    content: '+';
-  }
-`
-
 export const Container = styled.div`
   width: 100%;
+  font-size: 12px;
   display: flex;
   justify-content: center;
-  font-size: 12px;
 `
 
 export const ContentContainer = styled.div`
   width: 90%;
 `
-
-export const CoinData = styled.div`
-  /* outline: 1px solid yellow; */
-  margin: 5px;
-  
-  flex: 1 1 auto;
-  background: ${(props) => props.theme.secondary};
-  padding-inline: 36px;
-  padding-block: 18px;
-  border-radius: 25px;
-`
-
-export const CoinDataItem = styled.div`
-  margin-bottom: 5px;
-`
-
-export const CoinDataLabel = styled.span`
-  color: ${(props) => props.color || ''};
-  font-weight: 700;
-`
-
-export const CoinInfo = styled.div`
-  /* outline: 1px solid yellow; */
-  font-size: 1rem;
-  width: 100%;
-  text-align: center;
-`
-
-export const CoinInfoWrap = styled.div`
-  flex: 1 1 auto;
-  margin: 5px;
-  
-  background: ${(props) => props.theme.secondary};
-  display: flex;
-  align-items: center;
-  padding-inline: 36px;
-  padding-block: 18px;
-  border-radius: 25px; 
-`
-
-export const CoinHomepage = styled.div` 
-  margin-top: 10px;
-  font-size: 12px;
-  & a:link, a:visited {
-    text-decoration: none;
-    color: ${(props) => props.theme.mainFont};
-    padding-block: 4px;
-    padding-inline: 10px;
-    background: ${(props) => props.theme.primary};
-    border-radius: 10px;
-  } 
-`
-
-export const CoinPrices = styled.div`
-  /* outline: 1px solid yellow; */
-  flex: 1 1 auto;
-  margin: 5px;
-  background: ${(props) => props.theme.secondary};
-  padding-inline: 36px;
-  padding-block: 18px;
-  border-radius: 25px;
-`
-
-export const CoinSummary = styled.div`
-  /* outline: 1px solid red; */
-  margin-block: 5px;
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  flex-wrap: wrap;
-`
-
 export const Description = styled.div`
-  /* outline: 1px solid green; */
-  margin-inline: 5px;
   background: ${(props) => props.theme.secondary};
+  margin-inline: 5px;
   padding-inline: 36px;
   padding-block: 18px;
-  border-radius: 25px; 
-
-  & a:link, a:visited {
+  border-radius: 25px;
+  & a:link,
+  a:visited {
     text-decoration: none;
     color: #2172e5;
-    
     background: ${(props) => props.theme.primary};
     border-radius: 10px;
-  } 
+  }
 `
 
 export const H1 = styled.h1`
@@ -122,44 +37,8 @@ export const H2 = styled.h1`
   margin-block: 10px;
 `
 
-export const Hr = styled.hr`
-  margin-block: 10px;
-  width: 250px;
-`
-
-export const Img = styled.img`
-  width: 60px;
-  padding: 24px;
-  background: ${(props) => props.theme.primary};
-`
-
-export const ImgWrap = styled.div`
-  width: 100%;
-  margin-bottom: 10px;
-  display: flex;
-  justify-content: center;
-  border-radius: 20px;
-`
-
 export const LinkList = styled.div`
   margin-block: 20px;
   display: flex;
   flex-wrap: wrap;
-`
-
-export const LinkListItem = styled.div`
-  font-size: 12px;
-  margin: 5px;
-  flex: 1 1 auto;
-  display: flex;
-  justify-content: center;
-  padding-block: 12px;
-  padding-inline: 18px;
-  background: ${(props) => props.theme.secondary};
-  border-radius: 10px;
-
-  & a:link, a:visited {
-    text-decoration: none;
-    color: ${(props) => props.theme.mainFont};
-  } 
 `
--- src/pages/Coin/Coin.js
+++ src/pages/Coin/Coin.js
@@ -1,60 +1,24 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getCoinData } from 'store/coin/actions.js'
-import { ColumnCurrentPrice, LoadingBox, PercentDisplay } from 'components'
-
+import { CoinSummary, LinkItem, LoadingBox } from 'components'
 import {
-  BluePlus,
-  CoinData,
-  CoinDataItem,
-  CoinDataLabel,
-  CoinHomepage,
-  CoinInfo,
-  CoinPrices,
-  CoinSummary,
   Container,
   ContentContainer,
   Description,
   H1,
   H2,
-  Hr,
   LinkList,
-  Img,
-  ImgWrap,
-  CoinInfoWrap,
-  LinkListItem,
 } from './Coin.css'
-
-import { keyGen } from 'utils'
 
 const Coin = (props) => {
   const coinId = props.match.params.name
-  const { currency } = useSelector((state) => state.config)
   const dispatch = useDispatch()
   const { isLoading, data, error } = useSelector((state) => state.coin)
-
   useEffect(() => {
     dispatch(getCoinData(coinId))
     // eslint-disable-next-line
   }, [coinId])
-
-  const { name, image, symbol, links, market_data } = data || {}
-
-  const {
-    ath,
-    ath_change_percentage,
-    ath_date,
-    atl,
-    atl_change_percentage,
-    atl_date,
-    circulating_supply,
-    current_price,
-    fully_diluted_valuation,
-    market_cap,
-    market_cap_change_percentage_24h_in_currency,
-    max_supply,
-    total_volume,
-  } = market_data || {}
 
   return (
     <Container>
@@ -64,165 +28,31 @@ const Coin = (props) => {
         {error && <div>API ERROR</div>}
         {!isLoading && !error && data && (
           <>
-            <CoinSummary>
-              <CoinInfoWrap>
-                <CoinInfo>
-                  <ImgWrap>
-                    <Img src={image.large} alt="" />
-                  </ImgWrap>
-                  {name} ({symbol.toUpperCase()})
-                  
-                  {links.homepage.length > 0 && <CoinHomepage>
-                    <a href={links.homepage[0]} key={keyGen()}>{links.homepage[0]}</a>
-                  </CoinHomepage>}
-                
-                </CoinInfo>
-                
-              </CoinInfoWrap>
-
-              <CoinPrices>
-                <div>
-                  <span style={{ fontSize: '36px', fontWeight: 600 }}>
-                    <ColumnCurrentPrice price={current_price[currency]} />
-                  </span>
-                  {data.market_data.price_change_percentage_24h}%
-                  <div>@styled-icons/bootstrap/Stack</div>
-                </div>
-                <div>
-                  All Time High:
-                  <ColumnCurrentPrice price={ath[currency]} />
-                </div>
-                <div>{ath_change_percentage[currency].toFixed(2)}%</div>
-                <div>{ath_date[currency].slice(0, 10)}</div>
-                <Hr />
-                <div>
-                  All Time Low:
-                  <ColumnCurrentPrice price={atl[currency]} />
-                </div>
-                <div>{atl_change_percentage[currency].toFixed(2)}%</div>
-                <div>{atl_date[currency].slice(0, 10)}</div>
-              </CoinPrices>
-              <CoinData>
-                <CoinDataItem>
-                  <BluePlus />
-                  <CoinDataLabel>Market Cap: </CoinDataLabel>
-                  <ColumnCurrentPrice price={market_cap[currency]} />
-                  {` ${market_cap_change_percentage_24h_in_currency[currency]}`}
-                  %
-                </CoinDataItem>
-
-                <CoinDataItem>
-                  <BluePlus />
-                  <CoinDataLabel>Fully Diluted Valuation: </CoinDataLabel>
-                  <ColumnCurrentPrice
-                    price={fully_diluted_valuation[currency]}
-                  />
-                </CoinDataItem>
-
-                <CoinDataItem>
-                  <BluePlus />
-                  <CoinDataLabel>Volume 24h: </CoinDataLabel>
-                  <ColumnCurrentPrice price={total_volume[currency]} />
-                </CoinDataItem>
-
-                <CoinDataItem>
-                  <BluePlus />
-                  <CoinDataLabel>Volume / Market: </CoinDataLabel>
-                  {(total_volume[currency] / market_cap[currency])
-                    .toString()
-                    .slice(0, 8) + '...'}
-                </CoinDataItem>
-
-                <Hr />
-
-                <CoinDataItem>
-                  <BluePlus />
-                  <CoinDataLabel color="#1ad761">Total Volume: </CoinDataLabel>
-                  {`${Math.round(
-                    total_volume[currency] / current_price[currency],
-                  )} ${data.symbol.toUpperCase()}`}
-                </CoinDataItem>
-
-                <CoinDataItem>
-                  <BluePlus />
-                  <CoinDataLabel>Circulating Supply: </CoinDataLabel>
-                  {`${circulating_supply} ${data.symbol.toUpperCase()}`}
-                </CoinDataItem>
-
-                <CoinDataItem>
-                  <BluePlus />
-                  <CoinDataLabel color="#2172e5">Max Supply: </CoinDataLabel>
-                  {`${max_supply || Infinity} ${data.symbol.toUpperCase()}`}
-                </CoinDataItem>
-
-                <PercentDisplay
-                  marginTop={8}
-                  val1={total_volume[currency] / current_price[currency]}
-                  val2={circulating_supply}
-                  total={max_supply || Infinity}
-                />
-
-                {/* <div>-------------</div>
-                <div>24hr High: {data.market_data.high_24h[currency]}</div>
-                <div>24hr Low: {data.market_data.low_24h[currency]}</div>
-                <div>
-                  24h Price Change %:{' '}
-                  
-                </div>
-                <div>
-                  7d Price Change %:{' '}
-                  {data.market_data.price_change_percentage_7d}%
-                </div>
-                <div>
-                  14d PC%: {data.market_data.price_change_percentage_14d}%
-                </div>
-                <div>
-                  30d PC%: {data.market_data.price_change_percentage_30d}%
-                </div>
-                <div>
-                  60d PC% {data.market_data.price_change_percentage_60d}%
-                </div>
-                <div>
-                  200d PC%: {data.market_data.price_change_percentage_200d}%
-                </div>
-                <div>
-                  1yr PC%: {data.market_data.price_change_percentage_1y}%
-                </div>
-                <div>
-                  Market Cap 24h C%:
-                  {data.market_data.market_cap_change_percentage_24h}%
-                </div>
-                <div>
-                  Market Cap 24h C% in currency
-                  {
-                    data.market_data
-                      .market_cap_change_percentage_24h_in_currency[currency]
-                  }
-                  %
-                </div>
-
-                <div>----------</div> */}
-              </CoinData>
-            </CoinSummary>
-
+            <CoinSummary data={data} />
             <H2>Description:</H2>
+            <Description
+              dangerouslySetInnerHTML={{ __html: data.description.en }}
+            />
 
-            <Description dangerouslySetInnerHTML={{ __html: data.description.en }} />
-
-            <LinkList>
-                {data.links.blockchain_site.length >= 3 &&
-                (<><LinkListItem>
-                      <a href={data.links.blockchain_site[0]} key={keyGen()}>{data.links.blockchain_site[0]}</a>
-              </LinkListItem>
-              <LinkListItem>
-                      <a href={data.links.blockchain_site[1]} key={keyGen()}>{data.links.blockchain_site[1]}</a>
-              </LinkListItem>
-              <LinkListItem>
-                      <a href={data.links.blockchain_site[2]} key={keyGen()}>{data.links.blockchain_site[2]}</a>
-              </LinkListItem></>)
-                }
-              
-            </LinkList>
+            {data.links.blockchain_site.length >= 3 && (
+              <LinkList>
+                <LinkItem
+                  url={data.links.blockchain_site[0]}
+                  themeColor="secondary"
+                  padding="12px 18px"
+                />
+                <LinkItem
+                  url={data.links.blockchain_site[1]}
+                  themeColor="secondary"
+                  padding="12px 18px"
+                />
+                <LinkItem
+                  url={data.links.blockchain_site[2]}
+                  themeColor="secondary"
+                  padding="12px 18px"
+                />
+              </LinkList>
+            )}
           </>
         )}
       </ContentContainer>
