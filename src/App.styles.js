5df5273548075a96d3e0c0404efce62edd7f2a0f
Scott Million <scottmillion@gmail.com>
2021-10-29 11:45:17 +0700

    testing * and body width, height settings

--- src/App.styles.js
+++ src/App.styles.js
@@ -3,14 +3,18 @@ import { createGlobalStyle } from 'styled-components'
 export const GlobalStyle = createGlobalStyle`
   * {
     margin: 0;
+    padding: 0;
+    box-sizing: border-box;
   }
+
+
   body {
     font-size: 18px;
     color: ${(props) => props.theme.mainFont};
     background: ${(props) => props.theme.secondary};
     font-family: 'Poppins', sans-serif;
     min-height: 100vh;
-    min-width: 100vw;
+    
   }
 
   
