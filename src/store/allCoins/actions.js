0dfef7165a19cfae51432286f0d281c1cefbc31a
Scott Million <scottmillion@gmail.com>
2021-11-09 08:33:40 +0700

    Removed whitespace, lines

--- src/store/allCoins/actions.js
+++ src/store/allCoins/actions.js
@@ -19,8 +19,6 @@ export const getCoinsData = (queryOrder) => async (dispatch, getState) => {
     const { data } = await axios(
       `
       https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=50&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d
-
-      
       `,
     )
 
