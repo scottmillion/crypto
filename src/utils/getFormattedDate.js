36f10a16b10a4f8e27733bd3377666f0b6bcacf5
Scott Million <scottmillion@gmail.com>
2021-11-12 09:58:16 +0700

    Refactored so can take in a date string or will use todays date

--- src/utils/getFormattedDate.js
+++ src/utils/getFormattedDate.js
@@ -1,13 +1,17 @@
-export function getFormattedDate() {
-  const today = new Date()
-  const year = today.getFullYear()
-  let day = today.getDate().toString()
-  let month = (today.getMonth() + 1).toString()
+export function getFormattedDate(dateString) {
+  let date = new Date()
+  if (dateString) {
+    date = new Date(dateString)
+  }
+
+  const year = date.getFullYear()
+  let day = date.getDate().toString()
+  let month = (date.getMonth() + 1).toString()
   if (day.length === 1) {
     day = '0' + day
   }
   if (month.length === 1) {
     month = '0' + month
   }
-  return `${year}-${month}-${day}`
+  return `${month}-${day}-${year}`
 }
